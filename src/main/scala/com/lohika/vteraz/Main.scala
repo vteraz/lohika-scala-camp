5260ad552e765bd967bc3dfaa461517f8fb15fba
terazmint <terazvasyl@gmail.com>
2019-04-14 15:20:22 +0300

    initial commit

--- src/main/scala/com/lohika/vteraz/Main.scala
+++ src/main/scala/com/lohika/vteraz/Main.scala
@@ -1,28 +1,6 @@
 package com.lohika.vteraz
 
-import com.lohika.vteraz.service.CrimeService.CrimeDto
-import com.lohika.vteraz.data.{CsvReader, CsvReaderImpl}
-import com.lohika.vteraz.service.CrimeService
-
 object Main {
   def main(args: Array[String]): Unit = {
-    implicit val fileReader: CsvReader = new CsvReaderImpl(getFilePath(args))
-
-    val data: Seq[(String, List[CrimeDto])] = CrimeService.getTop5TheftLocations()
-
-    data.foreach(data => {
-      println("-----------------------------------------------------------------------------------")
-      println(s"${data._1}: ${data._2.size}")
-      println("Thefts:")
-      data._2.foreach(theft => println(s"ID: ${theft.id}"))
-    })
-  }
-
-  private def getFilePath(args: Array[String]): String = {
-    if(args.length < 1) {
-      Console.err.println("Path to file with Crimes should be provided")
-      sys.exit(1)
-    }
-    args(0)
   }
 }
---
+++ src/test/scala/com/lohika/vteraz/RetryTest.scala
@@ -0,0 +1,8 @@
+package com.lohika.vteraz
+
+import org.scalatest.{DiagrammedAssertions, FunSuite}
+
+class RetryTest extends FunSuite with DiagrammedAssertions {
+  test("Test retry") {
+  }
+}
