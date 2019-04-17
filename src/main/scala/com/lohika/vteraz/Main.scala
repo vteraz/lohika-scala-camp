203d735a516a1d9c5e18f6e3520e60c87525dc43
terazmint <terazvasyl@gmail.com>
2019-04-18 00:23:48 +0300

    refactoring

--- src/main/scala/com/lohika/vteraz/Main.scala
+++ src/main/scala/com/lohika/vteraz/Main.scala
@@ -1,87 +1,33 @@
 package com.lohika.vteraz
 
 import com.typesafe.scalalogging.LazyLogging
-import scalaj.http.{Http, HttpResponse}
 
 import scala.annotation.tailrec
 import scala.concurrent.duration._
 
 object Main extends LazyLogging {
 
-  val randomHttpRequest: () => Int = () => {
-    val randomUrl = "http://httpbin.org/status/100,200,300,400,500"
-    logger.info(s"Performing request to $randomUrl")
-
-    try {
-      val response: HttpResponse[String] = Http(randomUrl).timeout(2000, 5000).asString
-      logger.info(s"Response code - ${response.code}")
-
-      response.code
-    } catch {
-      case _: Exception => -1
-    }
-  }
-
-  def main(args: Array[String]): Unit = {
-    safeRetry[Int](randomHttpRequest, res => res == 200, List(0.seconds, 2.seconds, 4.seconds))
-
-    /////////////////////////////////////////////////////////////////////////////
-    try {
-      retry[Double](() => math.random(), res => res < 0.1, List(0.seconds, 1.seconds, 1.seconds))
-    } catch {
-      case _: FailedToPerformActionException => println("Unable to perform action after 3 retries")
+    def main(args: Array[String]): Unit = {
+        val result = retry[Double](() => math.random(), res => res < 0.1, List(1.seconds, 2.seconds, 3.seconds))
+        println(result)
     }
 
-    //////////////////////////////////////////////////////////////////////////////
-    retry[Double](() => math.random(), res => res < 0.1, List(0.seconds, 1.seconds, 1.seconds))
-  }
-
-  /**
-    * Performs action specified amount of times and try to get suitable result.
-    *
-    * <b>If no suitable result found - throws exception</b>
-    *
-    * @param action        Action to perform
-    * @param acceptResults Predicate to test whether result of action is valid
-    * @param retries       Number of tries and timeout between them
-    * @throws FailedToPerformActionException In case no valid result achieved
-    */
-  @tailrec
-  def retry[A](action: () => A,
-               acceptResults: A => Boolean,
-               retries: List[FiniteDuration]): A = {
-    retries match {
-      case Nil => throw new FailedToPerformActionException()
-      case x :: tail =>
-        logger.info(s"Sleeping $x and performing action")
-        Thread.sleep(x.toMillis)
+    /**
+      * Performs action specified amount of times and try to get suitable result.
+      *
+      * @param action        Action to perform
+      * @param acceptResults Predicate to test whether result of action is valid
+      * @param retries       Number of tries and timeout between them
+      */
+    @tailrec
+    def retry[A](action: () => A,
+                 acceptResults: A => Boolean,
+                 retries: List[FiniteDuration]): A = {
         val result = action.apply()
-        if (acceptResults.apply(result)) result else retry(action, acceptResults, tail)
+        if (acceptResults.apply(result) || retries.isEmpty) result
+        else {
+            Thread.sleep(retries.head.toMillis)
+            retry(action, acceptResults, retries.tail)
+        }
     }
-  }
-
-  /**
-    * Performs action specified amount of times and try to get suitable result.
-    *
-    * <b>If no suitable result found - returns last result of action no matter whether it's correct</b>
-    *
-    * @param action        Action to perform
-    * @param acceptResults Predicate to test whether result of action is valid
-    * @param retries       Number of tries and timeout between them
-    */
-  @tailrec
-  def safeRetry[A](action: () => A,
-                   acceptResults: A => Boolean,
-                   retries: List[FiniteDuration]): A = {
-    retries match {
-      case Nil => throw new FailedToPerformActionException() // this should never be happened
-      case x :: tail =>
-        logger.info(s"Sleeping $x and performing action")
-        Thread.sleep(x.toMillis)
-        val result = action.apply()
-        if (acceptResults.apply(result) || tail.isEmpty) result else safeRetry(action, acceptResults, tail)
-    }
-  }
-
-  class FailedToPerformActionException() extends RuntimeException("Failed to perform action") {}
 }
--- src/test/scala/com/lohika/vteraz/RetryTest.scala
+++ src/test/scala/com/lohika/vteraz/RetryTest.scala
@@ -1,20 +1,40 @@
 package com.lohika.vteraz
 
-import com.lohika.vteraz.Main.FailedToPerformActionException
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{DiagrammedAssertions, FunSuite}
 
 import scala.concurrent.duration._
 
 class RetryTest extends FunSuite with DiagrammedAssertions with MockFactory {
-  test("Test retry stopped when result is valid") {
-    val result = Main.retry[Int](() => 1 + 1, res => res % 2 == 0, List(0.seconds, 1.seconds))
-    assert(result == 2)
-  }
+    test("Test if first result valid, then no retries") {
+        val mockAcceptResults = mockFunction[Int, Boolean]
+        mockAcceptResults expects 2 returning true repeated 1 times
 
-  test("Test exception thrown when all retries failed") {
-    intercept[FailedToPerformActionException] {
-      Main.retry[Int](() => 1 + 1, res => res % 3 == 0, List(0.seconds, 1.seconds))
+        val result = Main.retry[Int](() => 1 + 1, mockAcceptResults, List(0.seconds, 1.seconds))
+        assert(result == 2)
     }
-  }
+
+    test("Test if all retries invalid, then last result returned") {
+        val mockAcceptResults = mockFunction[Int, Boolean]
+        mockAcceptResults expects 2 returning false repeated 3 times
+
+        val result = Main.retry[Int](() => 1 + 1, mockAcceptResults, List(0.seconds, 1.seconds))
+        assert(result == 2)
+    }
+
+    test("Test no retries specified - valid result") {
+        val mockAcceptResults = mockFunction[Int, Boolean]
+        mockAcceptResults expects 2 returning true repeated 1 times
+
+        val result = Main.retry[Int](() => 1 + 1, mockAcceptResults, List.empty)
+        assert(result == 2)
+    }
+
+    test("Test no retries specified - invalid result") {
+        val mockAcceptResults = mockFunction[Int, Boolean]
+        mockAcceptResults expects 2 returning false repeated 1 times
+
+        val result = Main.retry[Int](() => 1 + 1, mockAcceptResults, List.empty)
+        assert(result == 2)
+    }
 }
