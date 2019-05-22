61d9e0eb63aa5b78ffec4feb7a0e07693f32f99f
Vasyl Teraz <vteraz@n-ix.com>
2019-05-22 11:16:20 +0300

    SESSION-4 extend tests

--- src/main/scala/com/lohika/vteraz/route/ErrorHandlingSupport.scala
+++ src/main/scala/com/lohika/vteraz/route/ErrorHandlingSupport.scala
@@ -4,14 +4,15 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.{HttpEntity, HttpResponse, StatusCodes}
 import akka.http.scaladsl.model.StatusCodes.InternalServerError
 import akka.http.scaladsl.server.Directives.complete
-import akka.http.scaladsl.server.{ExceptionHandler, MalformedRequestContentRejection, RejectionHandler}
+import akka.http.scaladsl.server.{ExceptionHandler, MalformedRequestContentRejection, RejectionHandler, ValidationRejection}
 import com.lohika.vteraz.DbException
 import com.lohika.vteraz.model.ErrorResponseModel
 import com.typesafe.scalalogging.LazyLogging
 import spray.json.DefaultJsonProtocol.{jsonFormat3, _}
+import spray.json.RootJsonFormat
 
 object ErrorHandlingSupport extends LazyLogging {
-  implicit val errorResponseJsonFormat = jsonFormat3(ErrorResponseModel)
+  implicit val errorResponseJsonFormat: RootJsonFormat[ErrorResponseModel] = jsonFormat3(ErrorResponseModel)
 
   implicit def getExceptionHandler: ExceptionHandler =
     ExceptionHandler {
@@ -21,11 +22,17 @@ object ErrorHandlingSupport extends LazyLogging {
         complete(InternalServerError.intValue, ErrorResponseModel(InternalServerError.intValue, "GENERAL_EXCEPTION", a.getMessage))
     }
 
-  implicit def getRejectionHandler =
+  implicit def getRejectionHandler: RejectionHandler =
 
     RejectionHandler.newBuilder()
       .handle { case MalformedRequestContentRejection(message, _) =>
-        complete(400, ErrorResponseModel(StatusCodes.BadRequest.intValue, "BAD_REQUEST", message))
+        complete(StatusCodes.BadRequest.intValue, ErrorResponseModel(StatusCodes.BadRequest.intValue, "BAD_REQUEST", message))
+      case ValidationRejection(message, _) => {
+        complete(StatusCodes.BadRequest.intValue,
+          ErrorResponseModel(StatusCodes.BadRequest.intValue, "BAD_REQUEST", message))
+      }
+      case _ =>
+        complete(StatusCodes.BadRequest.intValue, ErrorResponseModel(StatusCodes.BadRequest.intValue, "BAD_REQUEST", ""))
       }
       .result()
 
--- src/test/scala/com/lohika/vteraz/RegisterUserTest.scala
+++ src/test/scala/com/lohika/vteraz/RegisterUserTest.scala
@@ -14,15 +14,17 @@ import com.lohika.vteraz.service.UserService
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfterEach, Matchers, WordSpec}
 import spray.json.DefaultJsonProtocol.{jsonFormat3, _}
+import spray.json.RootJsonFormat
 
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
 
 class RegisterUserTest extends WordSpec with Matchers with ScalaFutures with BeforeAndAfterEach
   with ScalatestRouteTest {
   implicit val exceptionHandler: ExceptionHandler = ErrorHandlingSupport.getExceptionHandler
   implicit val rejectionHandler: RejectionHandler = ErrorHandlingSupport.getRejectionHandler
   implicit val userService: UserService[Future] = new UserService[Future](new SlickH2UserRepository(DataSource.db))
-  implicit val errorResponseJsonFormat = jsonFormat3(ErrorResponseModel)
+  implicit val errorResponseJsonFormat: RootJsonFormat[ErrorResponseModel] = jsonFormat3(ErrorResponseModel)
 
   val userRoute = new UserRoute(userService)
   DataSource.init()
@@ -49,7 +51,7 @@ class RegisterUserTest extends WordSpec with Matchers with ScalaFutures with Bef
       }
     }
     "return BadRequest in case username already exists" in {
-      userService.registerUser(CreateUserRequest("John", Some("a"), "user@mail.com"))
+      Await.ready(userService.registerUser(CreateUserRequest("John", Some("a"), "user@mail.com")), 2.second)
 
       val jsonRequest = ByteString(
         s"""
@@ -70,6 +72,26 @@ class RegisterUserTest extends WordSpec with Matchers with ScalaFutures with Bef
         status shouldEqual StatusCodes.BadRequest
       }
     }
+    "return existing User by ID" in {
+      val result = Await.result(userService.registerUser(CreateUserRequest("John1", Some("a"), "user@mail.com")), 2.second)
+      val createUserRequest = HttpRequest(
+        HttpMethods.GET,
+        uri = s"/user?id=${result.right.get}")
+
+      createUserRequest ~> Route.seal(userRoute.routes) ~> check {
+        status shouldEqual StatusCodes.OK
+        responseAs[String] shouldEqual "{\"address\":\"a\",\"email\":\"user@mail.com\",\"id\":3,\"username\":\"John1\"}"
+      }
+    }
+    "return NotFound if user not exists" in {
+      val createUserRequest = HttpRequest(
+        HttpMethods.GET,
+        uri = s"/user?id=100")
+
+      createUserRequest ~> Route.seal(userRoute.routes) ~> check {
+        status shouldEqual StatusCodes.NotFound
+      }
+    }
     "return BadRequest in case username is not specified" in {
       val jsonRequest = ByteString(
         s"""
@@ -86,7 +108,7 @@ class RegisterUserTest extends WordSpec with Matchers with ScalaFutures with Bef
 
       createUserRequest ~> Route.seal(userRoute.routes) ~> check {
         status shouldEqual StatusCodes.BadRequest
-        responseAs[ErrorResponseModel] shouldEqual ErrorResponseModel(400,"BAD_REQUEST","Object is missing required member 'username'")
+        responseAs[ErrorResponseModel] shouldEqual ErrorResponseModel(400, "BAD_REQUEST", "Object is missing required member 'username'")
       }
     }
     "return BadRequest in case username contain special chars" in {
@@ -101,12 +123,12 @@ class RegisterUserTest extends WordSpec with Matchers with ScalaFutures with Bef
 
       val createUserRequest = HttpRequest(
         HttpMethods.POST,
-        uri = "/user1",
+        uri = "/user",
         entity = HttpEntity(MediaTypes.`application/json`, jsonRequest))
 
       createUserRequest ~> Route.seal(userRoute.routes) ~> check {
         status shouldEqual StatusCodes.BadRequest
-        responseAs[String] shouldEqual "requirement failed: username must not be empty AND contain only alphanumeric characters"
+        responseAs[String] shouldEqual "{\"code\":400,\"message\":\"requirement failed: username must not be empty AND contain only alphanumeric characters\",\"status\":\"BAD_REQUEST\"}"
       }
     }
     "return BadRequest in case email is not well-formatted" in {
@@ -126,7 +148,7 @@ class RegisterUserTest extends WordSpec with Matchers with ScalaFutures with Bef
 
       createUserRequest ~> Route.seal(userRoute.routes) ~> check {
         status shouldEqual StatusCodes.BadRequest
-        responseAs[String] shouldEqual "requirement failed: username must not be empty AND contain only alphanumeric characters"
+        responseAs[String] shouldEqual "{\"code\":400,\"message\":\"requirement failed: username must not be empty AND contain only alphanumeric characters\",\"status\":\"BAD_REQUEST\"}"
       }
     }
   }
